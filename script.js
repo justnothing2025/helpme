5d45a6a6b9ce77fdeaaad43899838de7005e17c5
justnothing2025 <samplenothing2025oc25@gmail.com>
2026-07-01 05:56:26 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+const counters = document.querySelectorAll('.counter');
+
+const observer = new IntersectionObserver(entries => {
+
+    entries.forEach(entry => {
+
+        if(entry.isIntersecting){
+
+            const counter = entry.target;
+
+            const target = +counter.dataset.target;
+
+            let current = 0;
+
+            const increment = target / 100;
+
+            const updateCounter = () => {
+
+                if(current < target){
+
+                    current += increment;
+
+                    counter.innerText = Math.ceil(current);
+
+                    requestAnimationFrame(updateCounter);
+
+                }else{
+
+                    if(target === 98){
+
+                        counter.innerText = target + "%";
+                    
+                    }else{
+                    
+                        counter.innerText = target + "+";
+                    
+                    }
+
+                }
+
+            };
+
+            updateCounter();
+
+            observer.unobserve(counter);
+
+        }
+
+    });
+
+});
+
+counters.forEach(counter => {
+
+    observer.observe(counter);
+
+});
+
+
+
+// FAQ
+
+const items = document.querySelectorAll(".faq-item");
+
+items.forEach(item => {
+
+item.querySelector(".faq-question").addEventListener("click", () => {
+
+item.classList.toggle("active");
+
+});
+
+});
